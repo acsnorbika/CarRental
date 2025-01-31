0dbc22636d15daa2f281fa5d4cc48cd85ab19c84
Andor1298 <palinkasandor@turr.hu>
2025-01-31 19:38:29 +0100

    autok class

--- CarRental/Autok.cs
+++ CarRental/Autok.cs
@@ -8,5 +8,27 @@ namespace CarRental
 {
     internal class Autok
     {
+        public string Marka { get; private set; }
+        public string Tipus { get; private set; }
+        public int Evjarat { get; private set; }
+        public decimal Ar { get; private set; }
+
+        public Auto(string marka, string tipus, int evjarat, decimal ar)
+        {
+            Marka = marka;
+            Tipus = tipus;
+            Evjarat = evjarat;
+            Ar = ar;
+        }
+
+        public string GetInsertValues()
+        {
+            return $"'{Marka}', '{Tipus}', {Evjarat}, {Ar}";
+        }
+
+        public override string ToString()
+        {
+            return $"{Marka} {Tipus} ({Evjarat}) - {Ar:C}";
+        }
     }
 }
